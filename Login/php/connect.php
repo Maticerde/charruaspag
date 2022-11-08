1ea54c1c0207fae04c8d6f38b7edbcc98e423895
Maticerde <maticerde@gmail.com>
2022-11-07 23:03:54 -0300

    elimine login de la carpeta de resktsoftware

--- Login/php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost:3306";
-    $username = "root";
-    $password = "";
-    $db_name = "vinos_charruas";
-    
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
