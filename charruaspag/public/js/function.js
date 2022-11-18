8871335ea38b41ce37d34534c8e8b77a91db37ed
German Guerrero <german.guerrero.213@gmail.com>
2022-11-18 18:00:02 -0300

    charruaspag

---
+++ charruaspag/public/js/function.js
@@ -0,0 +1,231 @@
+let carticon = document.querySelector("#cart-icon");
+carticon.addEventListener("click", cartanimation);
+
+desplegables = [];
+let desplegado = false;
+function desplegar(nodo) {
+  desplegables.push(nodo);
+  desplegables.forEach((element) => {
+    if (document.getElementById(element + "-desp").classList.contains("open")) {
+      document.getElementById(element + "-desp").classList.remove("open");
+    }
+  });
+  desplegable = document.getElementById(nodo + "-desp");
+  if (desplegado == false) {
+    // se da delay al cambiarse de un desplegable a otro, evita el delay cuando es el primero en abrirse
+    desplegable.classList.toggle("open");
+    desplegado = true;
+  } else {
+    setTimeout(() => desplegable.classList.toggle("open"), 600);
+  }
+}
+
+function cerrar_nodo() {
+  desplegable.classList.remove("open");
+  desplegables.length = 0;
+  desplegado = false;
+}
+
+function cartanimation() {
+  //funcion de despliegue de carrito
+  let panel = document.querySelector("#cart").classList.toggle("toggled");
+  let carticon = document
+    .querySelector("#cart-icon")
+    .classList.toggle("toggled");
+  let arrow = document.querySelector("#arrow").classList.toggle("toggled");
+}
+
+var carrito = [];
+var total = 0;
+title = document.getElementById("carrito-title").style.display = "none";
+totalcount = document.getElementById("totalcount").style.display = "none";
+compraboton = document.getElementById("compraboton").style.display = "none";
+borrar = document.getElementById("vaciar").style.display = "none";
+
+if ((carrito.length = 1)) {
+  carticon.style.opacity = "0";
+  carticon.style.pointerEvents = "none";
+}
+
+function acarrear(numprod, nombre, precio, stock, imagen, pais) {
+  if (total === 0 && stock > 0) {
+    cartanimation();
+    carticon.style.opacity = "0.8";
+    carticon.style.pointerEvents = "";
+  }
+
+  var agrego = true;
+  title = document.getElementById("carrito-title").style.display = "";
+  totalcount = document.getElementById("totalcount").style.display = "";
+  borrar = document.getElementById("vaciar").style.display = "";
+  compraboton = document.getElementById("compraboton").style.display = "";
+
+  objeto = {
+    name: nombre,
+    price: precio,
+    pais: pais,
+    cant: 1,
+    stock: stock,
+    imagen: imagen
+  };
+  if (stock == 0) {
+    agrego = false;
+  }
+  if (carrito.length > 0) {
+    carrito.forEach((element) => {
+      // en caso de que haya algo en el carrito recorro los elementos
+      if (element.name == objeto.name) {
+        // si el objeto seleccionado ya existe en el carrito la flag 'agrego' da falso
+        if (element.cant >= element.stock) {
+          // en caso de que la cantidad agregada al carro sea mayor al stock del producto..
+          alert("Ya alcanzó el stock maximo de ese producto");
+        } else {
+          // en caso de que no supere el stock
+          element.cant++; // se agrega otra unidad al mismo producto
+          total += parseInt(objeto.price); // el precio de la unidad extra se agrega al total
+        }
+        agrego = false; // si el objeto seleccionado ya existe en el carrito la flag 'agrego' da falso
+      }
+    });
+  }
+
+  if (agrego) {
+    // si la flag 'agrego' es verdadero
+    carrito.push(objeto); // se agrega al carrito el objeto
+    total += parseInt(objeto.price); // se suma al total el objeto agregado al carrito
+  }
+
+  carro = document.getElementById("carro-content");
+  carro.innerText = "";
+
+  carrito.forEach((element) => {
+    // por cada elemento en el carrito, se agrega una línea con la información del producto
+    carro.innerHTML +=
+    '<div id="cart_item"><img src=\"' + element.imagen + '\"><p1>'+ element.cant +' x $ '+ element.price +'</p1><p3>'+ element.name +'</p3><p4>'+ element.pais +'</p4></div>'
+  });
+
+  totalcount = document.getElementById("totalcount");
+  totalcount.innerText = "TOTAL: $ " + total; // se escribe el valor total
+}
+
+function generar_compra() {
+  carrito.forEach((element) => {
+    fetch_async_compra(element.name, element.stock, element.cant);
+  });
+}
+
+function fetch_async_compra(name, stock, cant) {
+  const data = new FormData();
+  data.set("name", name);
+  data.set("stock", stock);
+  data.set("cant", cant);
+
+  fetch("controllers/Stock_Controller.php", {
+    method: "POST",
+    body: data,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw "Error";
+      }
+    })
+    .then(function (texto) {
+      console.log(texto);
+      load_shop();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function vaciarcarrito() {
+  total = 0;
+  carrito.length = 0;
+  carro.innerText = "";
+  totalcount.innerText = "";
+  borrar = document.getElementById("vaciar").style.display = "none";
+  title = document.getElementById("carrito-title").style.display = "none";
+  compraboton = document.getElementById("compraboton").style.display = "none";
+  carticon.style.pointerEvents = "none";
+  carticon.style.opacity = "0";
+  cartanimation();
+}
+
+searchbar = document.getElementById("keywords").addEventListener('keydown', (functiona) => {
+  setTimeout(() => search(), 50);
+});
+
+function search() {
+  const data = new FormData();
+  keywords = document.querySelector("#keywords").value;
+  data.set("keyword_post", keywords);
+
+  //ya tengo el dato escrito cuando toco el boton, falta enviar por post a implement para actualizar el select y despues loadear shop nuevamente
+  fetch("controllers/IndexVinos_Controller.php", {
+    method: "POST",
+    body: data,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw "Error";
+      }
+    })
+    .then(function (texto) {
+      //load_shop()
+      load_filter_shop(texto);
+      //alert(texto);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function load_filter_shop(filtro) {
+  const market = document.querySelector(".productos-gallery");
+  market.innerHTML = filtro;
+}
+
+totalcount = document.getElementById("totalcount");
+totalcount.innerText = "TOTAL: $ " + total; // se escribe el valor total
+
+arrow = document.getElementById("arrow");
+menu = document.getElementById("menu");
+let auxflag = true;
+
+function myScrollFunc() {
+  let y = window.scrollY;
+  if (y > 1100) {
+    arrow.style.top = "60px";
+    if (!menu.classList.contains("llevarmenu") && auxflag) {
+      menu.style.top = "-57px";
+      menu.classList.add("llevarmenu");
+      setTimeout(() => menu.style.top = "0px", 100);
+    } 
+  } else {
+    arrow.style.top = "-50px";
+    if (menu.classList.contains("llevarmenu") && auxflag) {
+      menu.style.top = "-57px";
+      setTimeout(() => menu.classList.remove("llevarmenu"), 500);
+      setTimeout(() => menu.style.top = "0", 500);
+      setTimeout(() => menu.style.top = "", 1000);
+      auxflag = false; // previene el inicio de otra animacion hasta que termine la actual
+      setTimeout(() => auxflag = true, 1000);
+    }
+  }
+}
+window.addEventListener("scroll", myScrollFunc);
+window.addEventListener("load", myScrollFunc);
+function scrollto() {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+function alertacarrito(){
+  alert(" Para comprar productos, primero debes iniciar sesión");
+}
---
+++ charruaspag/views/panel_admin/panel_admin.php
@@ -0,0 +1,285 @@
+<?php
+session_start();
+if(isset($_SESSION['setAdmin'])){
+    echo "<script> alert('Solo los administradores pueden ingresar a este lugar, no compartir esta informacion');</script> ";
+
+    // $usuarioingresado = $_SESSION['nombredeusuario'];
+    //  echo "<h1> bienvenido: $usuarioingresado </h1>";
+    // header('location: /charruaspag/views/panel_admin/panel_admin.php');
+
+} else{
+    header('location: /charruaspag/views/login/index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="icon" href="../../src/charruas logo.png">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, minimum-scale=1">
+  <link href="../../public/css/style-admin.css" rel="stylesheet" type="text/css" />
+  <script src="../../public/js/admin_vinos_mod.js"></script>
+  <script src="../../public/js/admin_bodegas_mod.js"></script>
+
+  <title>Panel Admin</title>
+</head>
+
+<body>
+  <img id=fondo src="../../src/foto3.jpg">
+  <div id="menu">
+    <section id="texto1">A d m i n i s t r a c i ó n</section>
+    <a href="http://localhost/charruaspag/index.php">
+      <section id="charruas-texto"> Charrúas </section>
+    </a>
+  </div>
+  <section id="text-prod" onclick="slide(1)"><span>&#10132;</span> P R O D U C T O S
+    <div id="text-prod-preventclick"
+      onclick="event.cancelBubble=true;if (event.stopPropagation) event.stopPropagation();"></div>
+  </section>
+
+  <section id="text-user" onclick="slide(0)"><span>&#10132;</span> U S U A R I O S
+    <div id="text-user-preventclick"
+      onclick="event.cancelBubble=true;if (event.stopPropagation) event.stopPropagation();"></div>
+  </section>
+
+  <div class="box" id="modvino-bax">
+    <h3>MODIFICAR PRODUCTO</h3>
+    <span id="mensaje_modv"></span>
+    <form action="../../controllers/admin_ModVino_Controller.php" method="POST" id="modvino-form">
+      <section id="input_grid_modv">
+        <input type="number" id="in_codigo_vino" name="in_codigo_vino" style="display: none">
+        <input type="text" id="in_imagenString_mod" name="in_imagenString_mod" style="display: none">
+        <label class="input_label">
+          <input class="inputs_mod" type="text" id="in_nombre_vino" name="in_nombre_vino" maxlength="20" required>
+          <p2 class="input_texto_modv"> Nombre </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="number" min="0" id="in_precio" name="in_precio" required>
+          <p2 class="input_texto_modv"> Precio </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="number" min="0" id="in_stock" name="in_stock" required>
+          <p2 class="input_texto_modv"> Stock </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="text" id="in_pais" name="in_pais" maxlength="20" required>
+          <p2 class="input_texto_modv"> País </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="text" id="in_region" name="in_region" maxlength="50" required>
+          <p2 class="input_texto_modv"> Región </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="number" min="0" id="in_cosecha" name="in_cosecha" required>
+          <p2 class="input_texto_modv"> Cosecha </p2>
+        </label>
+        <label class="input_label">
+          <input class="inputs_mod" type="text" list="in_bodega_vino" name="in_bodega_vino" required>
+          <p2 class="input_texto_modv"> ID Bodega </p2>
+          <datalist id="in_bodega_vino">
+            <script src="../../public/js/admin_bodegas_form_load.js"></script>
+          </datalist>
+        </label>
+        <label id="imagen_label" class="input_label">
+          <input class="inputs_mod" type="file" id="in_imagen_mod" name="in_imagen_mod" accept=".png">
+          <p2 id="nombre_imagen_mod"> Imagen </p2>
+          <img id="preview" src="">
+        </label>
+      </section>
+      <button type="button" name="modvino-button-preventclick" id="modvino-button-preventclick" style="display: none"></button>
+      <button type="button" onclick="validate_submit_modv();" name="modvino-button" id="modvino-button"> ENVIAR </button>
+    </form>
+  </div>
+
+  <div class="box" id="modbodega-bax">
+    <h3>MODIFICAR BODEGA</h3>
+    <span id="mensaje_modb"></span>
+      <form action="../../controllers/admin_ModBodega_Controller.php" method="post" id="modbodega-form">
+      <section id="input_grid_modb">
+      <input type="number" id="in_codigo_bodega" name="in_codigo_bodega" style="display: none">
+          <label class="input_label">
+            <input class="inputs_modb" type="text" id="in_nombre_bodega" name="in_nombre_bodega" maxlength="40" required>
+            <p2 class="input_texto_modb"> Nombre </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="text" id="in_email" name="in_email" maxlength="50" required>
+            <p2 class="input_texto_modb"> Email </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="text" id="in_direccion" name="in_direccion" maxlength="70" required>
+            <p2 class="input_texto_modb"> Dirección </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="text" id="in_pais" name="in_pais" maxlength="25" required>
+            <p2 class="input_texto_modb"> País </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="number" min="0" id="in_postal" name="in_postal" required>
+            <p2 class="input_texto_modb"> Codigo Postal </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="text" id="in_ciudad" name="in_ciudad" maxlength="25" required>
+            <p2 class="input_texto_modb"> Ciudad </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="number" min="0" id="in_telefono" name="in_telefono" required>
+            <p2 class="input_texto_modb"> Teléfono </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_modb" type="number" min="0" id="in_cuenta" name="in_cuenta" required>
+            <p2 class="input_texto_modb"> Cuenta </p2>
+          </label>
+        </section>
+        <button type="button" name="modbodega-button-preventclick" id="modbodega-button-preventclick" style="display: none"></button>
+        <button type="button" onclick="validate_submit_modb();" name="modbodega-button" id="modbodega-button"> ENVIAR </button>
+      </form>
+  </div>
+
+  <div id="vinos-list-label">
+
+    <span id="info-section" onclick="desplegarmod();">
+    </span>
+    <span id="info-efecto">
+    </span>
+    <section id="info-vino">
+      <img class="info" id="v_img" />
+      <div class="info" id="v_nombre"></div>
+      <section id="upperinfo">
+        <div class="info" id="v_pais"></div>
+        <div class="info" id="v_region"></div>
+        <div class="info" id="v_cosecha"></div>
+      </section>
+      <section id="lowerinfo">
+        <div class="info" id="v_precio"></div>
+        <div class="info" id="v_stock"></div>
+      </section>
+    </section>
+    <section id="info-bodega">
+      <div class="infob" id='b_nombre'></div>
+      <section id='upperinfo'>
+        <div class="infob" id='b_pais'></div>
+        <div class="infob" id='b_ciudad'></div>
+        <div class="infob" id='b_cuenta'></div>
+      </section>
+      <section id='lowerinfo'>
+        <div class="infob" id='b_direccion'></div>
+        <div class="infob" id='b_codpostal'></div>
+        <div class="infob" id='b_email'></div>
+      </section>
+    </section>
+    <h1>LISTA DE PRODUCTOS:</h1>
+    <section id="vinos-list">
+      <script src="../../public/js/admin_vinos_list_function.js"></script>
+      <script src="../../public/js/admin_mostrar_info.js"></script>
+    </section>
+    <form id="vinos-order" method="POST">
+      <script src="../../public/js/admin_vinos_orden.js"></script>
+      <input type="radio" name="orden" onclick="load_vinos(); load_orden_style('orden1')" value="Nombre_Vino" checked>
+      <p1 class="ord" id="orden1">A &#10132; Z</p1></input>
+      <input type="radio" name="orden" onclick="load_vinos(); load_orden_style('orden2')" value="Codigo_Vino DESC">
+      <p1 class="ord" id="orden2">Recientes</p1></input>
+      <input type="radio" name="orden" onclick="load_vinos(); load_orden_style('orden3')" value="Codigo_Vino ASC">
+      <p1 class="ord" id="orden3">Antiguos</p1></input>
+    </form>
+  </div>
+  <section id="grid-functions">
+    <div class="box" id="addvino-box">
+      <h3>AGREGAR PRODUCTO</h3>
+      <span id="mensaje"></span>
+      <form action="../../controllers/admin_AddVino_Controller.php" method="POST" id="addvino-form">
+        <section id="input_grid">
+          <label class="input_label">
+            <input class="inputs" type="text" id="in_nombre_vino" name="in_nombre_vino" maxlength="20" required>
+            <p2 class="input_texto"> Nombre </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs" type="number" min="0" id="in_precio" name="in_precio" required>
+            <p2 class="input_texto"> Precio </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs" type="number" min="0" id="in_stock" name="in_stock" required>
+            <p2 class="input_texto"> Stock </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs" type="text" id="in_pais" name="in_pais" maxlength="20" required>
+            <p2 class="input_texto"> País </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs" type="text" id="in_region" name="in_region" maxlength="50" required>
+            <p2 class="input_texto"> Región </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs" type="number" min="0" id="in_cosecha" name="in_cosecha" required>
+            <p2 class="input_texto"> Cosecha </p2>
+          </label>
+          <label class="input_label">
+            <input type="text" list="in_bodega_vino" class="inputs" name="in_bodega_vino" required>
+            <p2 class="input_texto"> ID Bodega </p2>
+            <datalist id="in_bodega_vino">
+              <script src="../../public/js/admin_bodegas_list_load.js"></script>
+            </datalist>
+          </label>
+          <label id="imagen_label" class="input_label">
+            <input class="inputs" type="file" id="in_imagen" name="in_imagen" accept=".png" required>
+            <p2 id="nombre_imagen"> Imagen </p2>
+            <img id="preview" src="">
+          </label>
+        </section>
+        <button name="addvino-button" id="addvino-button"> ENVIAR </button>
+      </form>
+    </div>
+    <div class="box" id="addbodega-box">
+      <h3>AGREGAR BODEGA</h3>
+      <span id="mensaje"></span>
+      <form action="../../controllers/admin_AddBodega_Controller.php" method="post" id="addbodega-form">
+        <section id="input_grid">
+          <label class="input_label">
+            <input class="inputs_mod" type="text" id="in_nombre_bodega" name="in_nombre_bodega" maxlength="40" required>
+            <p2 class="input_texto"> Nombre </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="text" id="in_email" name="in_email" maxlength="50" required>
+            <p2 class="input_texto"> Email </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="text" id="in_direccion" name="in_direccion" maxlength="70" required>
+            <p2 class="input_texto"> Dirección </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="text" id="in_pais" name="in_pais" maxlength="25" required>
+            <p2 class="input_texto"> País </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="number" min="0" id="in_postal" name="in_postal" required>
+            <p2 class="input_texto"> Codigo Postal </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="text" id="in_ciudad" name="in_ciudad" maxlength="25" required>
+            <p2 class="input_texto"> Ciudad </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="number" min="0" id="in_telefono" name="in_telefono" required>
+            <p2 class="input_texto"> Teléfono </p2>
+          </label>
+          <label class="input_label">
+            <input class="inputs_mod" type="number" min="0" id="in_cuenta" name="in_cuenta" required>
+            <p2 class="input_texto"> Cuenta </p2>
+          </label>
+        </section>
+        <button name="addbodega-button" id="addbodega-button"> ENVIAR </button>
+      </form>
+    </div>
+    <div class="box" id="user-box">
+      <h3> AGREGAR USUARIO </h3>
+    </div>
+
+    <div class="box" id="usermod-box">
+      <h3>MODIFICAR USUARIO</h3>
+    </div>
+  </section>
+  <script src="../../public/js/admin_function.js"></script>
+</body>
+
+</html>
