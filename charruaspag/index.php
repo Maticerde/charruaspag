e924a1eaccd7133582c19d2fdec8f7e211200527
Maticerde <maticerde@gmail.com>
2022-11-18 22:09:03 -0300

    18/11

--- charruaspag/index.php
+++ charruaspag/index.php
@@ -9,14 +9,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vinos Charrúas</title>
   <link href="public/css/style.css" rel="stylesheet" type="text/css">
+  <link href="public/css/carrito.css" rel="stylesheet" type="text/css">
+  <link href="public/css/desplegables.css" rel="stylesheet" type="text/css">
   <script src="public/js/script_load.js"></script>
-  
+
 </head>
 
 <div id="menu">
-
+<img id="cart-icon" src="src/carticon.png"><span id="cart-count"></span></img>
+<form id="gototienda" action="http://localhost/charruaspag/views/market/market.php">
+    <button id="gototienda">Tienda</button>
+  </form>
 <section id="usuariologeado">
-    <?php // muestra el email del usuario
+<?php // muestra el email del usuario
       if(isset($_SESSION["nombredeusuario"]))
       {
         echo $_SESSION["nombredeusuario"]; 
@@ -24,8 +29,7 @@
         echo $_SESSION["setAdmin"];
       }
     ?>
-    </section>
-  
+  </section>
 
   <?php
 if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, aparecerá el apartado de cerrar sesión
@@ -62,22 +66,21 @@ if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, apare
 </div>
 <?php include 'views/desplegables/desplegables.php';?>
 <img id="arrow" src="src/house_arrow.png" onclick="scrollto()"/>
-<img id="cart-icon" src="src/carticon.png"></img>
 <div id="cart">
   <h1 id="carrito-title"> Carrito Charrúas </h1>
-  <img id="vaciar" onclick="vaciarcarrito();" src="src/trashicon.png"></img>
   <img id="close_cart" onclick="cartanimation();" src="src/right_arrow.png"></img>
   <section id="carro-content"></section>
   <div id="totalcount"></div>
-  <button id="compraboton" <?php 
-  if(isset($_SESSION["nombredeusuario"])){
-    echo 'onclick="generar_compra(); load_shop(); vaciarcarrito(); " ';
-  } elseif(isset($_SESSION["setAdmin"])) {
-    echo 'onclick="generar_compra(); load_shop(); vaciarcarrito(); " ';
-  } else {
-    echo 'onclick="alertacarrito();"';
-  }
-  ?>>comprar</button>
+  <section id="cart-buttons">
+    <button id="compraboton" <?php 
+    if(isset($_SESSION["nombredeusuario"])){
+      echo 'onclick="generar_compra(); load_shop(); vaciarcarrito(); " '; }
+    else {
+      echo 'onclick="alertacarrito();"';
+    }
+    ?>>Comprar</button>
+    <button type="button" id="vaciar" onclick="vaciarcarrito();"><p2>Vaciar</p2><img src="src/trashicon.png"/></button>
+  </section>
 </div>
 <section id="texto1"> Un vino, ㅤ una Historia
   <p> “El mejor vino no es necesariamente el más caro, sino el que se comparte.” </p>
@@ -90,8 +93,8 @@ if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, apare
 </div>
 <img id="bsdetalle" src="src/wine-sommalier-drawing.png"> </img>
 <img id="deco-bstext" src="src/deco-bs.png"> </img>
-<section id="advertbox-i"> <a href="https://www.google.com.uy/maps/place/La+Casa+Violeta/@-34.8951846,-56.06,17z/data=!4m5!3m4!1s0x959f87968e4b3317:0x842ebec130f23abd!8m2!3d-34.8951662!4d-56.0607054" target="_blank"><img src="src/cubo_cena2_1400x.progressive.jpg"/></a></section>
-<section id="advertbox-d"> <a href=""><img src="src/wine-bottle-label-mockup-db.jpg"/></a></section>
+<!-- <section id="advertbox-i"> <a href="https://www.google.com.uy/maps/place/La+Casa+Violeta/@-34.8951846,-56.06,17z/data=!4m5!3m4!1s0x959f87968e4b3317:0x842ebec130f23abd!8m2!3d-34.8951662!4d-56.0607054" target="_blank"><img src="src/cubo_cena2_1400x.progressive.jpg"/></a></section>
+<section id="advertbox-d"> <a href=""><img src="src/wine-bottle-label-mockup-db.jpg"/></a></section> -->
 <div id="boxbs">
   <img id="bsfondo" src="src/wine-splash.png"> </img>
   <span id="bestsellers">
@@ -117,9 +120,8 @@ if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, apare
 </section>
 <div id="slidertext"></div>
 <section class="gallery-wrapper">
-  <input type="text" id="keywords" name="keywords" size="30" maxlength="30" placeholder="Buscar productos">
-  </button>
-  <section class="productos-gallery"></section>
+  <h1>¡Últimas Oportunidades!</h1>
+<section class="productos-gallery"></section>
 </section>
 <div id="box3">
   <div class="contactobox">
@@ -128,7 +130,7 @@ if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, apare
     <p2 id="charruas-texto2"> Charrúas </p2>
     </a>
     <ul>
-      <li id="contacto" onclick="desplegar(id)"> Contacto </li>
+      <li id="contacto"><a href="mailto:charruas.soporte@gmail.com">Contacto</a></li>
       <li id="fdepago" onclick="desplegar(id)"> Formas de pago </li>
       <li id="poldevolucion" onclick="desplegar(id)"> Política de Devolución </li>
       <li id="polprivacidad" onclick="desplegar(id)"> Políticas de Privacidad </li>
@@ -138,5 +140,6 @@ if(isset($_SESSION["nombredeusuario"])) { // si el cliente inició sesion, apare
 <div id="copyright">
   <section id="copy"> RESCATE SOFTWARE © Todos los derechos reservados.<section>
 </div>
-<section id="darken"> </section>
-<script src="public/js/function.js"></script>
+<script src="public/js/carrito.js"></script>
+<script src="public/js/scroll-function.js"></script>
+<script src="public/js/desplegables.js"></script>
---
+++ charruaspag/public/js/market_load.js
@@ -0,0 +1,53 @@
+function load_shop() {
+    const market = document.querySelector(".productos-gallery");
+    // se inicia load de productos
+    market.innerHTML = "";
+    fetch("../../controllers/MarketVinos_Controller.php").then(function (response) {
+      return response.text().then(function (text) {
+        //alert(text);
+        market.innerHTML = text;
+      });
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function (event) {
+    load_shop();
+  });
+
+  searchbar = document.getElementById("keywords").addEventListener('keydown', (e) => {
+    setTimeout(() => search(), 50);
+  });
+
+  function search() {
+    const data = new FormData();
+    keywords = document.querySelector("#keywords").value;
+    data.set("keyword_post", keywords);
+  
+    //ya tengo el dato escrito cuando toco el boton, falta enviar por post a implement para actualizar el select y despues loadear shop nuevamente
+    fetch("../../controllers/MarketVinos_Controller.php", {
+      method: "POST",
+      body: data,
+    })
+      .then(function (response) {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw "Error";
+        }
+      })
+      .then(function (texto) {
+        //load_shop()
+        load_filter_shop(texto);
+        //alert(texto);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  
+
+
+  function load_filter_shop(filtro) {
+    const market = document.querySelector(".productos-gallery");
+    market.innerHTML = filtro;
+  }
